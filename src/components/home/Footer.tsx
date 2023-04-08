e79a7b6311144ddad9da8c16daf098887ee00f20
Nam Hoang Nguyen <nam.nh.2068@aptechlearning.edu.vn>
2023-04-08 18:25:57 +0700

    [update] home layout

--- src/components/home/Footer.tsx
+++ src/components/home/Footer.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div className='w-full px-10 py-3 bg-gray-200 border-gray-300'>
+    <div>
+      <div className="h-[256px] flex w-full flex px-[6.8%]">
+        <div className="w-1/3 mt-24 mr-12">
+          <p className="text-xl mb-4">Lorem ipsum is placeholder text commonly used in the graphic, print, Lorem ipsum is placeholder text commonly used in the graphic, print</p>
+          <div className="flex">
+            <img className="w-[30px] h-[30px] mr-4" src="/facebook.svg" alt="" />
+            <img className="w-[30px] h-[30px] mr-4" src="/twitter.svg" alt="" />
+            <img className="w-[30px] h-[30px] mr-4" src="/linkedln.svg" alt="" />
+            <img className="w-[30px] h-[30px] mr-4" src="/instagram.svg" alt="" />
+            <img className="w-[30px] h-[30px] mr-4" src="/youtube.svg" alt="" />
+          </div>
+        </div>
+        <div className="w-1/6 flex flex-col mt-10">
+          <h5 className="font-semibold text-xl text-[#1C1C1C] mb-2">Giới thiệu</h5>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+        </div>
+        <div className="w-1/6 flex flex-col mt-10">
+          <h5 className="font-semibold text-xl text-[#1C1C1C] mb-2">Đối tác</h5>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+        </div>
+        <div className="w-1/6 flex flex-col mt-10">
+          <h5 className="font-semibold text-xl text-[#1C1C1C] mb-2">Thông tin</h5>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+        </div>
+        <div className="w-1/6 flex flex-col mt-10">
+          <h5 className="font-semibold text-xl text-[#1C1C1C] mb-2">Người dùng</h5>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+          <a href="/">Lorem ipsum</a>
+        </div>
+      </div>
+      <div className="w-full h-[68px] px-[6.8%] flex items-center px-10 py-3 bg-gray-200 border-gray-300">
         <p>&copy; 2023 Royal Software.</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,15 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 
 const listCategory = [
-  { name: "Điện thoại di động", path: "/smartphones" },
-  { name: "Trang phục", path: "/smartphones" },
-  { name: "Đồ gia dụng", path: "/smartphones" },
-  { name: "Đồ điện tử", path: "/smartphones" },
-  { name: "Công cụ", path: "/smartphones" },
-  { name: "Thể thao", path: "/smartphones" },
-  { name: "Mẹ và bé", path: "/smartphones" },
-  { name: "Thú cưng", path: "/smartphones" },
-  { name: "Xem thêm", path: "/smartphones" },
+  { name: "Điện thoại di động", path: "/" },
+  { name: "Trang phục", path: "/" },
+  { name: "Đồ gia dụng", path: "/" },
+  { name: "Đồ điện tử", path: "/" },
+  { name: "Công cụ", path: "/" },
+  { name: "Thể thao", path: "/" },
+  { name: "Mẹ và bé", path: "/" },
+  { name: "Thú cưng", path: "/" },
+  { name: "Xem thêm", path: "/" },
 ];
 
 const listItems = [
@@ -24,17 +24,37 @@ const listItems = [
   { title: "Điện thoại", price: "2.000.000", svg: "/item-1.svg" },
 ]
 
+const listSuggestions = [
+  { description: "Áo cộc tay thương hiệu Coolmate", price: "300.000", svg: "/suggestion-1.svg" },
+  { description: "Áo ấm dành cho mùa đông lạnh giá", price: "1.000.000", svg: "/suggestion-1.svg" },
+  { description: "Suit nam cao cấp đến từ thương hiệu Việt", price: "500.000", svg: "/suggestion-1.svg" },
+  { description: "Ví da bò nhưng được làm từ nhựa tổng hợp", price: "100.000", svg: "/suggestion-1.svg" },
+  { description: "Balo cho học sinh hoặc du lịch", price: "200.000", svg: "/suggestion-1.svg" },
+  { description: "Quần đùi nam mùa hè mặc vào hơi nóng", price: "4.000.000", svg: "/suggestion-1.svg" },
+  { description: "Tai nghe Trung Quốc chất lượng cao", price: "2.000.000", svg: "/suggestion-1.svg" },
+  { description: "Cũng là balo vì không có hình thay thế", price: "2.000.000", svg: "/suggestion-1.svg" },
+  { description: "Chum đi đái dành cho trẻ em", price: "2.000.000", svg: "/suggestion-1.svg" },
+  { description: "Thùng rác thông minh Trung Quốc", price: "2.000.000", svg: "/suggestion-1.svg" },
+]
+
+const listService = [
+  { description: "Hàng lấy trực tiếp từ nhà phân phối", icon: "/service-icon-1.svg", svg: "/service-1.svg"},
+  { description: "Chăm sóc khách hàng tận tình", icon: "/service-icon-2.svg", svg: "/service-2.svg"},
+  { description: "Nhanh chóng, mọi lúc, mọi nơinơi", icon: "/service-icon-3.svg", svg: "/service-3.svg"},
+  { description: "An toàn và bảo mật trong vận chuyển", icon: "/service-icon-4.svg", svg: "/service-4.svg"}
+]
+
 export default function Home() {
   return (
-    <main className="overflow-x-auto">
+    <main className="overflow-x-auto bg-[#e0e0e0]">
       <div className="w-full px-[6.8%]">
         <h1 className="py-6 text-2xl font-bold text-[#1C1C1C]">Khám phá</h1>
-        <div className="w-full flex border border-gray-300 border-solid border-1px h-[400px] p-5 rounded-lg mb-6">
+        <div className="w-full flex border border-gray-300 border-solid border-1px h-[400px] p-5 rounded-lg mb-6 bg-[#FFFFFF]">
           <div className="w-1/6 flex flex-col justify-between mr-4">
             <ul>
               {listCategory.map((item, index) => (
-                <a key={index} href={item.path}>
-                  <li className="py-1.5 px-2 text-xl text-[#505050] hover:bg-[#E5F1FF] hover:text-[#1C1C1C] hover:font-semibold rounded-lg">
+                <a key={index} href={item.path} className="group hover:no-underline">
+                  <li className="py-1.5 px-2 text-xl text-[#505050] group-hover:bg-[#E5F1FF] group-hover:text-[#1C1C1C] group-hover:font-semibold rounded-lg">
                     {item.name}
                   </li>
                 </a>
@@ -94,14 +114,14 @@ export default function Home() {
           </div>
           <div className="container h-full flex flex-wrap bg-[#FFFFFF]">
             {listItems.map((item, index) => (
-              <div key={index} className="flex items-center justify-between px-14 border border-solid border-[1px] border-[#E0E0E0] justify-center w-1/4">
+              <div key={index} className="flex items-center justify-between px-14 border border-solid border-[1px] border-[#E0E0E0] justify-center w-1/4 group hover:cursor-pointer">
                 <div className="flex flex-col">
                   <h4 className="text-[#1C1C1C] font-semibold">{item.title}</h4>
                   <p className="text-[#8B96A5]">Giá từ</p>
                   <span className="text-[#8B96A5]">{item.price}</span>
                 </div>
                 <div>
-                  <img src={item.svg} alt="" />
+                  <img className="group-hover:contrast-125 hover:-translate-y-4" src={item.svg} alt="" />
                 </div>
               </div>
             ))}
@@ -110,8 +130,40 @@ export default function Home() {
       </div>
       <div className="w-full px-[6.8%]">
         <h1 className="py-6 text-2xl font-bold text-[#1C1C1C]">Mua đi mà</h1>
-        <div className="w-full flex border border-gray-300 border-solid border-[1px] h-[400px] rounded-lg">
-
+        <div className="w-full flex flex-wrap h-[640px]">
+          {listSuggestions.map((item, index) => (
+            <div key={index} className="w-1/5 h-[310px] hover:cursor-pointer hover:brightness-90">
+              <div  className="h-full p-2 m-2 rounded-lg bg-[#FFFFFF] border border-gray-300 border-solid border-[1px]">
+                <div className="flex p-4 items-center h-[200px] mb-2">
+                  <img className="w-full h-full" src={item.svg} alt="" />
+                </div>
+                <div className="flex flex-col px-6">
+                  <h4 className="text-[#1C1C1C] font-semibold">{item.price}</h4>
+                    <p className="text-[#8B96A5]">{item.description}</p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="w-full px-[6.8%]">
+        <h1 className="py-6 text-2xl font-bold text-[#1C1C1C]">Dịch vụ tiện ích khác</h1>
+        <div className="w-full flex flex-wrap h-[240px] mb-[125px]">
+          {listService.map((item, index) => (
+            <div key={index} className="relative w-1/4 h-[200px] hover:cursor-pointer hover:brightness-90">
+              <div className="m-2 rounded-lg bg-[#FFFFFF] border border-gray-300 border-solid border-[1px]">
+                <div className="h-[160px]">
+                  <img className="w-full bg-[#3f3f3f]" src={item.svg} alt="" />
+                </div>
+                <div className="p-6 w-2/3">
+                  <h3 className="text-[#1C1C1C] font-semibold text-xl">{item.description}</h3>
+                </div>
+              </div>
+              <div className="flex items-center justify-center absolute top-[72%] right-10 bg-[#D1E7FF] w-1/6 h-1/3 flex p-5 rounded-full border border-[2px] border-solid border-[#FFFFFF] hover:-translate-y-4">
+                <img src={item.icon} alt="" />
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </main>
